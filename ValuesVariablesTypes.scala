122782d07b96c305a89ed7d2fb63a9d269f12767
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2022-08-27 15:41:59 -0300

    Create ValuesVariablesTypes.scala

---
+++ ValuesVariablesTypes.scala
@@ -0,0 +1,21 @@
+package Playground
+
+object ValuesVariablesTypes extends App {
+  val x: Int =  42
+  println(x)
+
+  //val is immutable
+  //compiler can infer types
+
+  val x1 = 40
+  println(x1)
+
+  val aString: String = "Hello, World"
+  println(aString)
+
+  val aBoolean: Boolean = true
+  val aDouble: Double = 2.25
+  val aFloat: Float = 1.0f
+  val aChar: Char = 'A'
+
+}
